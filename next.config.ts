8f8258d175ab4af3dcbe41f8e8427f7d847b59ab
lang <bambang.career@gmail.com>
2025-08-19 11:54:20 +0700

    Update ftp dan config

--- next.config.ts
+++ next.config.ts
@@ -2,21 +2,22 @@ import type { NextConfig } from "next";
 import withPWAInit from "@ducanh2912/next-pwa";
 
 const withPWA = withPWAInit({
-  dest: "public", // Simpan service worker di folder public
+  dest: "public",
   register: true,
-  disable: process.env.NODE_ENV === "development", // Nonaktifkan PWA saat development
+  disable: process.env.NODE_ENV === "development",
 });
 
 const nextConfig: NextConfig = {
-
-
+  output: "export", // Tambah ini buat static export
+  trailingSlash: true, // Biar URL berakhir dengan /
   images: {
+    unoptimized: true, // Wajib buat static export
     remotePatterns: [
       {
-        protocol: 'https',
-        hostname: 'strapi.fairuzulum.me',
-        port: '',
-        pathname: '/uploads/**',
+        protocol: "https",
+        hostname: "strapi.fairuzulum.me",
+        port: "",
+        pathname: "/uploads/**",
       },
     ],
   },
